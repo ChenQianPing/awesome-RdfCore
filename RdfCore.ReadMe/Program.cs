18bd6d455b3c1a5289cc40e1688f62615b158420
ChenQianPing <13542427@qq.com>
2020-09-01 13:29:16 +0800

    添加项目文件。

---
+++ RdfCore.ReadMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RdfCore.ReadMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
